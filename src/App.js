47a3069937d609244d1a4d04d9c583e86f71565d
dtebas <dougtebas@gmail.com>
2020-07-28 22:09:39 -0300

    Fix bug

--- src/App.js
+++ src/App.js
@@ -11,17 +11,38 @@ function App() {
     <div>
       <Menu />
 
-    <BannerMain
-      videoTitle={dadosIniciais.categoria[0].videos[0].titulo}
-      url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é Front-end? Trabalhando na área"}
-    />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
 
-    <Carousel
-      ignoreFirstVideo
-      category={dadosIniciais.categorias[0]}
-    />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />
 
+      <Footer />
 
     </div>
   );
